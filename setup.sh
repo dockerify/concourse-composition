a18ad7e7888cb72956e147b80e08cfef44ef1710
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2017-05-16 13:50:23 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-lpass show 8953247532597087321 --notes | grep POSTGRES > .postgres-env
-lpass show 8953247532597087321 --notes | grep CONCOURSE > .concourse-env
+lpass show 8953247532597087321 --notes | grep POSTGRES > postgres.env
+lpass show 8953247532597087321 --notes | grep CONCOURSE > concourse.env
+
+PSQL_DB=$(lpass show 8953247532597087321 --notes | grep POSTGRES_DB | awk -F '=' '{print $2}')
+PSQL_USER=$(lpass show 8953247532597087321 --notes | grep POSTGRES_USER | awk -F '=' '{print $2}')
+PSQL_PASSWORD=$(lpass show 8953247532597087321 --notes | grep POSTGRES_PASSWORD | awk -F '=' '{print $2}')
+
+export POSTGRES_DB=${PSQL_DB}
+export POSTGRES_USER=${PSQL_USER}
+export POSTGRES_PASSWORD=${PSQL_PASSWORD}
 
 mkdir -p keys/web keys/worker
 
@@ -22,14 +30,16 @@ do
   docker-machine create --driver virtualbox worker$i || true
 done
 
-eval $(docker-machine env manager1)
+eval "$(docker-machine env manager1)"
 
-export MANAGER_IP=$(docker-machine env manager1 | grep HOST | awk -F '//' '{print $2}' | awk -F ':' '{print $1}' )
-export JOIN_TOKEN_MANAGER=$(docker-machine ssh manager1 docker swarm join-token manager | grep token | awk '{print $2}')
-export JOIN_TOKEN_WORKER=$(docker-machine ssh manager1 docker swarm join-token worker | grep token | awk '{print $2}')
+HOST_IP=$(docker-machine env manager1 | grep HOST | awk -F '//' '{print $2}' | awk -F ':' '{print $1}' )
+TOKEN_MANAGER=$(docker-machine ssh manager1 docker swarm join-token manager | grep token | awk '{print $2}')
+TOKEN_WORKER=$(docker-machine ssh manager1 docker swarm join-token worker | grep token | awk '{print $2}')
 
-export POSTGRES_DB=$(lpass show 8953247532597087321 --notes | grep POSTGRES_DB | awk -F '=' '{print $2}')
-export POSTGRES_PASSWORD=$(lpass show 8953247532597087321 --notes | grep POSTGRES_PASSWORD | awk -F '=' '{print $2}')
+export MANAGER_IP=${HOST_IP}
+export JOIN_TOKEN_MANAGER=${TOKEN_MANAGER}
+export JOIN_TOKEN_WORKER=${TOKEN_WORKER}
+
 export CONCOURSE_EXTERNAL_URL=${MANAGER_IP}:8000
 
 docker-machine ssh manager1 docker swarm init --advertise-addr "${MANAGER_IP}" || true
@@ -41,9 +51,9 @@ do
   docker-machine ssh worker$i docker swarm join --token "${JOIN_TOKEN_WORKER}" "${MANAGER_IP}":2377 || true
 done
 
-docker-machine ssh manager1 docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer || true
+docker-machine ssh manager1 docker run --name=portainer -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer || true
 
-docker-machine ssh manager1 docker run -d -p 5000:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer || true
+docker-machine ssh manager1 docker run --name=visualizer -d -p 5000:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer || true
 
 echo "open visualizer at http://${MANAGER_IP}:5000"
 
